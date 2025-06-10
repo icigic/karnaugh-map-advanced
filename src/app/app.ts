6b0d43bd64c47bde09c8180ed6adced46c68a9d9
Ivan Cigic <ivan.cigic@yahoo.com>
2025-06-10 06:24:50 +0200

    Add layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Content } from './content/content';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Content, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
